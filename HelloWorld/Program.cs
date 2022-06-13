bd19a3e093966176e293914eae916671ada098de
Jason Chen <chenjason971@gmail.com>
2022-06-13 10:37:09 -0500

    Finished Project

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# program to print Hello World
+using System;
+
+//namespace declaration
+namespace HelloWorld
+{
+    //Class Declaration
+    class Program
+    {
+        //Main Method
+        static void Main(string[] args)
+        {
+            //statement
+            //printing Hellow World!
+            Console.WriteLine("HelloWorld");
+
+            //To prevent the screen from running and closing quickly
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+
